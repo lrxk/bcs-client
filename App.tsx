282d5211f137026310783ff0ff10fb9130f52451
kevin leroux <kevin.leroux@etu.univ-lehavre.fr>
2022-11-17 12:25:39 +0100

    Screens and buttons

--- App.tsx
+++ App.tsx
@@ -1,13 +1,55 @@
 import { StripeProvider } from '@stripe/stripe-react-native';
-import CheckoutScreen from './CheckoutScreen';
 
-export default function App() {
+import CheckoutScreen from './CheckoutScreen';
+import BarCodeScannerScreen from './BarCodeScannerScreen';
+import BasketScreen from './BasketScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { Button, View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+
+
+const Stack = createStackNavigator();
+
+
+function HomeScreen({ navigation }: any) {
   return (
-    <StripeProvider
-      publishableKey="pk_test_51LxoKKHqMywXhbC79U1iNbpdJObZir61ouukdBjw1g5OVwnIdcLQmJju8uq5UDxDB8edvSCIT2sxVAVmzDkJ626I00m7XU3GGc"
-      merchantIdentifier="merchant.com.example"
-    >
-      <CheckoutScreen />
-    </StripeProvider>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button
+        title="Go to Checkout"
+        onPress={() => navigation.navigate('Checkout')}
+      />
+      <Button
+        title="Go to BarCodeScanner"
+        onPress={() => navigation.navigate('BarCodeScanner')}
+      />
+      <Button
+        title="Go to Basket"
+        onPress={() => navigation.navigate('Basket')}
+      />
+
+    </View>
+  );
+}
+export default function App({ navigation }: any) {
+  return (
+
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Checkout" component={CheckoutScreen} />
+        <Stack.Screen name="BarCodeScanner" component={BarCodeScannerScreen} />
+        <Stack.Screen name="Basket" component={BasketScreen} />
+
+      </Stack.Navigator>
+    </NavigationContainer>
+
+
+
+
+
+
+
   );
 }
