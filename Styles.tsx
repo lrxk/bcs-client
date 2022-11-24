df374c1f767e6ba9f2479c3cd0d4405ae3f20353
kevin leroux <kevin.leroux@etu.univ-lehavre.fr>
2022-11-24 23:30:09 +0100

    Stylesheet file

---
+++ Styles.tsx
@@ -0,0 +1,117 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+export const Styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        backgroundColor: '#fff',
+        padding: 10,
+        margin: 10,
+        borderRadius: 10,
+        borderWidth: 1,
+        borderColor: '#000',
+    },
+    textContainer: {
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        alignItems: 'flex-start',
+    },
+    textInput: {
+        height: 40,
+        borderColor: 'gray',
+        borderWidth: 1,
+        margin: 10,
+        padding: 10,
+    },
+    buttonContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+    text: {
+        fontSize: 20,
+        color: 'black',
+        textAlign: 'center',
+        marginTop: 20,
+    },
+    button: {
+        backgroundColor: '#000',
+        padding: 10,
+        margin: 10,
+        borderRadius: 10,
+    },
+    buttonText: {
+        fontSize: 20,
+        color: 'white',
+
+    },
+    header: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    headerText: {
+        fontSize: 30,
+        color: 'black',
+        textAlign: 'center',
+        marginTop: 20,
+    },
+    cartContainer: {
+        flex: 1,
+        flexDirection: 'column',
+    },
+    itemContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        backgroundColor: '#fff',
+        padding: 10,
+        margin: 10,
+        borderRadius: 10,
+        borderWidth: 1,
+        borderColor: '#000',
+    },
+    itemText: {
+        fontSize: 20,
+        color: 'black',
+        textAlign: 'center',
+        marginTop: 20,
+    },
+    totalContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    totalText: {
+        fontSize: 30,
+        color: 'black',
+        textAlign: 'center',
+        marginTop: 20,
+    },
+    barcodeContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    barcodeText: {
+        fontSize: 30,
+        color: 'black',
+        textAlign: 'center',
+        marginTop: 20,
+    },
+    barcodeScanner: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+
+});
