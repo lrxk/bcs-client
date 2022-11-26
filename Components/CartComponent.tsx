dec4bb3304bc69f378a69f5d442997ab18ddb27b
kevin leroux <kevin.leroux@etu.univ-lehavre.fr>
2022-11-26 14:50:54 +0100

    Visual rework

--- Components/CartComponent.tsx
+++ Components/CartComponent.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { View, Text, TouchableOpacity, ScrollView } from 'react-native';
+import { View, Text, TouchableOpacity, ScrollView, FlatList } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { Cart } from '../Types/Types';
 import { useState } from 'react';
 import { useEffect } from 'react';
-import {Styles} from '../Styles';
+import { Styles } from '../Styles';
 
 export default function CartComponent(props: { cart: Cart }) {
     const navigation = useNavigation();
@@ -18,48 +18,56 @@ export default function CartComponent(props: { cart: Cart }) {
         setTotal(total);
     }, [cart]);
     return (
-        <View style={Styles.container}>
-            <View style={Styles.header}>
-                <Text style={Styles.headerText}>Cart</Text>
-            </View>
-            <ScrollView style={Styles.cartContainer}>
-                {cart.items.map((item) => {
-                    return (
-                        <View style={Styles.itemContainer} key={item.id}>
-                            <Text style={Styles.itemText}>Item name:{item.name}</Text>
-                            <Text style={Styles.itemText}>Price: {item.price}</Text>
-                            <Text style={Styles.itemText}>Quantity:{item.quantity}</Text>
-                            <TouchableOpacity style={Styles.button} onPress={() => {
-                                let index = cart.items.findIndex((items) => items.id === item.id);
-                                if (index !== -1) {
-                                    cart.items[index].quantity++;
-                                }
-                                setCart({ ...cart });
-                            }}>
-                                <Text style={Styles.buttonText}>+</Text>
-                            </TouchableOpacity>
-                            <TouchableOpacity style={Styles.button} onPress={() => {
-                                let index = cart.items.findIndex((items) => items.id === item.id);
-                                if (index !== -1) {
-                                    cart.items[index].quantity--;
-                                }
-                                setCart({ ...cart });
-                            }}>
-                                <Text style={Styles.buttonText}>-</Text>
-                            </TouchableOpacity>
-                        </View>
-                    );
-                })}
-            </ScrollView>
-            <View style={Styles.totalContainer}>
-                <Text style={Styles.totalText}>Total: {total}</Text>
-            </View>
-            <View style={Styles.buttonContainer}>
-                <TouchableOpacity style={Styles.button} onPress={() => navigation.navigate('Checkout',{cart:cart})}>
-                    <Text style={Styles.buttonText}>Checkout</Text>
-                </TouchableOpacity>
-            </View>
-        </View>
+        <FlatList data={cart.items} renderItem={({ item }) => {
+            return (
+                <View style={Styles.itemContainer}>
+                    <View style={Styles.itemNameContainer}>
+                        <Text style={Styles.itemName}>{item.name}</Text>
+                    </View>
+                    <View style={Styles.itemPriceContainer}>
+                        <Text style={Styles.itemPrice}>${item.price}</Text>
+                    </View>
+                    <View style={Styles.itemQuantityContainer}>
+                        <Text style={Styles.itemQuantity}>Quantity: {item.quantity}</Text>
+                    </View>
+                    <TouchableOpacity style={Styles.button} onPress={() => navigation.navigate('Checkout', { cart: cart })}>
+                        <Text style={Styles.buttonText}>Checkout</Text>
+                    </TouchableOpacity>
+                    {/* increment quantity button */}
+                    <TouchableOpacity style={Styles.button} onPress={() => {
+                        let newCart = { ...cart };
+                        newCart.items.forEach((cartItem) => {
+                            // if the item is already in the cart, increment the quantity
+                            if (cartItem.id === item.id) {
+                                cartItem.quantity++;
+                            }
+                        });
+                        setCart(newCart);
+                    }}>
+                        <Text style={Styles.buttonText}>+</Text>
+                    </TouchableOpacity>
+                    {/* decrement quantity button */}
+                    <TouchableOpacity style={Styles.button} onPress={() => {
+                        let newCart = { ...cart };
+                        newCart.items.forEach((cartItem) => {
+                            // if the quantity is 1 and the user clicks the decrement button, remove the item from the cart
+                            if (cartItem.id === item.id && cartItem.quantity === 1) {
+                                newCart.items = newCart.items.filter((cartItem) => cartItem.id !== item.id);
+                            }
+                            if (cartItem.id === item.id) {
+                                cartItem.quantity--;
+                            }
+                        });
+                        setCart(newCart);
+                    }}>
+                        <Text style={Styles.buttonText}>-</Text>
+                    </TouchableOpacity>
+                </View>
+            )
+
+        }} />
+
+
     );
 }
 
