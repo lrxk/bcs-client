6ad45ada4c413122498d2eac6ad6572ac11d63b4
kevin leroux <kevin.leroux@etu.univ-lehavre.fr>
2022-11-27 17:46:30 +0100

    Move Stripe provider into app instead of CheckoutScreen

--- App.tsx
+++ App.tsx
@@ -1,17 +1,20 @@
 import CheckoutScreen from './Screens/CheckoutScreen';
 import BarCodeScannerScreen from './Screens/BarCodeScannerScreen';
 import CartScreen from './Screens/CartScreen';
+import PaidScreen from './Screens/PaidScreen';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { Button, View } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import React from 'react';
 import { Cart } from './Types/Types';
-
+import { StripeProvider } from '@stripe/stripe-react-native';
+import Constants from "expo-constants";
+const stripe_publishable_key = Constants.expoConfig.extra.stripe_publishable_key;
 
 // initialize empty cart
 export const cart: Cart = {
-  items: [  ],
+  items: [],
 };
 
 
@@ -24,7 +27,7 @@ function HomeScreen({ navigation }: any) {
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Button
         title="Go to Checkout"
-        onPress={() => navigation.navigate('Checkout',{cart:cart})}
+        onPress={() => navigation.navigate('Checkout', { cart: cart })}
       />
       <Button
         title="Add item to cart"
@@ -39,13 +42,19 @@ function HomeScreen({ navigation }: any) {
 }
 export default function App({ navigation }: any) {
   return (
-    
+
     <NavigationContainer>
+      <StripeProvider
+        publishableKey={stripe_publishable_key}
+        merchantIdentifier="merchant.com.example">
+
+      </StripeProvider>
       <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Checkout" component={CheckoutScreen} />
         <Stack.Screen name="BarCodeScanner" component={BarCodeScannerScreen} />
         <Stack.Screen name="Cart" component={CartScreen} />
+        <Stack.Screen name="Paid" component={PaidScreen} />
       </Stack.Navigator>
     </NavigationContainer>
 
